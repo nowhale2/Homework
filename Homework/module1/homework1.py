9633fa17c4b1d06cf4924216325fc83b35a7314c
nowhale2 <164938633+nowhale2@users.noreply.github.com>
2024-04-01 09:45:59 +0300

    Add files via upload

---
+++ Homework/module1/homework1.py
@@ -0,0 +1,6 @@
+num1 = 3
+num2 = 5
+sum = num1 + num2
+print(sum)
+result = sum + num1 * num2
+print(result)
---
+++ Homework/module1/homework2.py
@@ -0,0 +1,8 @@
+name = "Nikita"
+print("Name: ", name)
+age = 19
+print("Age: ", age)
+new_age = age - 2
+print("New age: ", new_age)
+is_student = True
+print("Is Students: ", is_student)
