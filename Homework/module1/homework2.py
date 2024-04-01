803cc1ec05543fabc59590e336de7842a21e2204
nowhale2 <164938633+nowhale2@users.noreply.github.com>
2024-04-01 13:52:34 +0300

    Add files via upload

--- Homework/module1/homework2.py
+++ Homework/module1/homework2.py
@@ -2,7 +2,7 @@ name = "Nikita"
 print("Name: ", name)
 age = 19
 print("Age: ", age)
-new_age = age - 2
-print("New age: ", new_age)
+age -= 2
+print("New age: ", age)
 is_student = True
 print("Is Students: ", is_student)
