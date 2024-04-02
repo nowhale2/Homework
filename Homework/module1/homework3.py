b98abc0662d3c8120e617cd598eaa8fd25668ca3
nowhale2 <164938633+nowhale2@users.noreply.github.com>
2024-04-02 10:30:46 +0300

    Add files via upload

---
+++ Homework/module1/homework3.py
@@ -0,0 +1,7 @@
+my_string = input("Напишите что-нибудь: ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(f'Первый символ строки: "{my_string[0]}"')
+print(f'Последний символ строки: "{my_string[0]}"')
