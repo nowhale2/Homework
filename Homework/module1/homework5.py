8b1ffc325baa524f4c0e1828ee60962301a524e2
nowhale2 <164938633+nowhale2@users.noreply.github.com>
2024-04-03 12:09:43 +0300

    homework5.py

--- Homework/module1/homework5.py
+++ Homework/module1/homework5.py
@@ -5,7 +5,7 @@ print("Last element: ", my_list[-1])
 print("Third, fourth, fifth elements: ", my_list[2:5])
 my_list[2] = 'pumpkin'
 print("Modified list: ", my_list)
-print('')       # Пустая строка для лучшей читаемости кода
+print('')       # Пустая строка для лучшей визуалициии результата
 my_dict = {'shoes': 'обувь', 'skirt': 'юбка', 'shorts': 'шорты', 'jeans': 'джинсы', 'socks': 'носки', 'coat': 'пальто'}
 print("Dictionary: ", my_dict)
 print("Translation: ", my_dict.get('shoes'))
