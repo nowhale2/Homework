f5ab1c1c733de154a70ff2f488f6d90926e08d0a
nowhale2 <164938633+nowhale2@users.noreply.github.com>
2024-05-09 00:58:27 +0300

    Update homework3.py

--- Homework/module5/homework3.py
+++ Homework/module5/homework3.py
@@ -6,9 +6,3 @@ class Building:
 
     def __eq__(self, other):
         return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
-
-
-House1 = Building()
-House2 = Building()
-if House1.__eq__(House2):
-    print('Они похожи')
