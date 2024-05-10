d6915d893edb17c58b04e4567ce75e4a0e66ebeb
nowhale2 <164938633+nowhale2@users.noreply.github.com>
2024-05-10 15:43:19 +0300

    Delete homework3.py

--- homework3.py
+++
@@ -1,8 +0,0 @@
-class Building:
-
-    def __init__(self, number_of_floors, building_type):
-        self.numberOfFloors = number_of_floors
-        self.buildingType = building_type
-
-    def __eq__(self, other):
-        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
