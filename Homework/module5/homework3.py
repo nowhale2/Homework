07bb29de75831db755471c772fcba9455a2e2cae
nowhale2 <164938633+nowhale2@users.noreply.github.com>
2024-05-10 15:43:53 +0300

    Add files via upload

--- Homework/module5/homework3.py
+++ Homework/module5/homework3.py
@@ -1,8 +1,8 @@
 class Building:
 
-    def __init__(self):
-        self.numberOfFloors = 1
-        self.buildingType = '1'
+    def __init__(self, number_of_floors, building_type):
+        self.numberOfFloors = number_of_floors
+        self.buildingType = building_type
 
     def __eq__(self, other):
         return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
