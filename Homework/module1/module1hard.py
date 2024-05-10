cadecfdac85449f2f23f5ae5ee9d8fa34cae8c2d
nowhale2 <164938633+nowhale2@users.noreply.github.com>
2024-05-10 15:46:10 +0300

    Update module1hard.py
    the variable «middle» has been moved out of the loop

--- Homework/module1/module1hard.py
+++ Homework/module1/module1hard.py
@@ -7,10 +7,10 @@ grades = ['5 3 3 5 4', '2 2 2 3', '4 5 5 2', '4 4 3', '5 5 5 4 5']
 students = {'Johnni', 'Bilbo', 'Steve', 'Kendrick', 'Aaron'}
 average_score = {}
 students = sorted(list(students))
+middle = 0
 for i in range(len(students)):
     grades[i] = grades[i].split(' ')
     sum = 0
-    middle = 0
     for j in range(len(grades[i])):
         sum += int(grades[i][j])
         middle = sum / len(grades[i])
